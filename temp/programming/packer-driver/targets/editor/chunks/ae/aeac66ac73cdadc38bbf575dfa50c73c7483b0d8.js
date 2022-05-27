85cc2d5fac6c7001722ef5fdb3c7271c7fc4525f
yanyongxing <1959555480@qq.com>
2022-05-27 18:44:36 +0800

    feat:创建项目

---
+++ temp/programming/packer-driver/targets/editor/chunks/ae/aeac66ac73cdadc38bbf575dfa50c73c7483b0d8.js
@@ -0,0 +1,35 @@
+System.register(["cc"], function (_export, _context) {
+  "use strict";
+
+  var _cclegacy, _decorator, Component, _dec, _class, _crd, ccclass, property, GameManager;
+
+  return {
+    setters: [function (_cc) {
+      _cclegacy = _cc.cclegacy;
+      _decorator = _cc._decorator;
+      Component = _cc.Component;
+    }],
+    execute: function () {
+      _crd = true;
+
+      _cclegacy._RF.push({}, "eee3cylgrxCD6HapV2N1OSv", "GameManager", undefined);
+
+      ({
+        ccclass,
+        property
+      } = _decorator);
+
+      _export("GameManager", GameManager = (_dec = ccclass('GameManager'), _dec(_class = class GameManager extends Component {
+        start() {}
+
+        update(deltaTime) {}
+
+      }) || _class));
+
+      _cclegacy._RF.pop();
+
+      _crd = false;
+    }
+  };
+});
+//# sourceMappingURL=aeac66ac73cdadc38bbf575dfa50c73c7483b0d8.js.map
---
+++ temp/programming/packer-driver/targets/editor/chunks/bf/bfec2e162ce7e1d49a80bc6938f6e458fdeaedc5.js
@@ -0,0 +1,35 @@
+System.register(["cc"], function (_export, _context) {
+  "use strict";
+
+  var _cclegacy, _decorator, Component, _dec, _class, _crd, ccclass, property, MoveSceneBg;
+
+  return {
+    setters: [function (_cc) {
+      _cclegacy = _cc.cclegacy;
+      _decorator = _cc._decorator;
+      Component = _cc.Component;
+    }],
+    execute: function () {
+      _crd = true;
+
+      _cclegacy._RF.push({}, "b1487fXNu5JD4RoACHJpiHt", "MoveSceneBg", undefined);
+
+      ({
+        ccclass,
+        property
+      } = _decorator);
+
+      _export("MoveSceneBg", MoveSceneBg = (_dec = ccclass('MoveSceneBg'), _dec(_class = class MoveSceneBg extends Component {
+        start() {}
+
+        update(deltaTime) {}
+
+      }) || _class));
+
+      _cclegacy._RF.pop();
+
+      _crd = false;
+    }
+  };
+});
+//# sourceMappingURL=bfec2e162ce7e1d49a80bc6938f6e458fdeaedc5.js.map
